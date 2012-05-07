bc15c0169593b141e70d1708eaebff8d47ef4009
Jack Dubie <jdubie@cs.stanford.edu>
2012-05-07 10:17:13 -0700

    make jackbook conditionally load

--- custom/jackbook.zsh
+++ custom/jackbook.zsh
@@ -1,3 +1,8 @@
+# for use on all CSADVISOR machines
+if [ -z "$JACKBOOK" ]; then
+  return
+fi
+
 ###########################################################
 ## Environment Variables and Aliases Specific
 ## to Jackbook
