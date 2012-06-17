eb78641271d1ad02e443bc90ab882ddd885262cc
Jack Dubie <jdubie@cs.stanford.edu>
2012-06-17 15:11:17 -0700

    Added binaries in current wd to PATH

--- custom/common.zsh
+++ custom/common.zsh
@@ -8,3 +8,6 @@ alias tree='$~ZSH_CUSTOM/scripts/tree.sh'
 
 # always keep node_modules/.bin in your path
 PATH=$PATH:./node_modules/.bin
+
+# put executables in your working directory in your path
+PATH:$PATH:.
