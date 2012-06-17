6611cea8b560b09b666dd0382607d7f53413a60a
Jack Dubie <jdubie@cs.stanford.edu>
2012-06-17 13:47:25 -0700

    Always keep node_modules/.bin in your path

--- custom/common.zsh
+++ custom/common.zsh
@@ -5,3 +5,6 @@ alias inpath='PATH=$PWD:$PATH'
 
 PATH=$PATH:~/.oh-my-zsh/scripts
 alias tree='$~ZSH_CUSTOM/scripts/tree.sh'
+
+# always keep node_modules/.bin in your path
+PATH=$PATH:./node_modules/.bin
