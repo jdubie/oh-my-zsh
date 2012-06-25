d92fafd758fda76bc31bf69d676a658fe6a312cd
Jack Dubie <jdubie@cs.stanford.edu>
2012-06-24 19:25:39 -0700

    -a
    wef

--- custom/common.zsh
+++ custom/common.zsh
@@ -11,3 +11,7 @@ PATH=$PATH:./node_modules/.bin
 
 # put executables in your working directory in your path
 #PATH:$PATH:. # TODO get this working
+
+# quickly open nodejs or kanso project files
+alias kk='$EDITOR kanso.json'
+alias pp='$EDITOR package.json'
